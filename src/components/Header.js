69b37f701b5129e9be19e48576005803e583bf6e
Prodigy_J <jknasumo@gmail.com>
2022-02-12 12:52:29 +0000

    Created mobile navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,20 @@
 import React from 'react';
 import classes from './Header.module.css';
 
-export default function Header() {
+export default function Header(props) {
+
+    const [menuOpen, setMenuOpen] = React.useState(false);
+
+    function toggleMenu() {
+        const list = document.querySelector(`.${classes.inlineList}`);
+        const hamburger = document.querySelector(`#hamburger`);
+        setMenuOpen(!menuOpen);
+        hamburger.classList.toggle('is-active');
+        list.classList.toggle(`${classes.active}`);
+        hamburger.classList.toggle(`${classes.isActive}`)
+        console.log(list)
+    }
+    
     return (
         <header className={classes.header}>
             <h3 className={classes.h3}>&lt; jerome n tokpa /&gt;</h3>
@@ -13,10 +26,10 @@ export default function Header() {
                 <li>about</li>
                 <li>contact</li>
             </ul>
-            <div className={classes.hamburger}>
-                <span className={classes.bar}></span>
-                <span className={classes.bar}></span>
-                <span className={classes.bar}></span>
+            <div id='hamburger' onClick={toggleMenu} className={classes.hamburger}>
+                <span id='bar1' className={classes.bar}></span>
+                <span id='bar2' className={classes.bar}></span>
+                <span id='bar3' className={classes.bar}></span>
             </div>
         </header>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,10 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import FaceCard from '../components/FaceCard';
 
-export default function Home() {
+export default function Home(props) {
     return (
         <div>
-            <Header />
+            <Header/>
             <FaceCard />
             <Footer />
         </div>
