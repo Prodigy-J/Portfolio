edc5e6b21c551831cf070652d9fc8127a4fbdf66
Prodigy_J <jknasumo@gmail.com>
2022-02-16 15:03:52 +0000

    Modified Project jsx and styles

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,6 +3,8 @@ import classes from "./Project.module.css";
 
 export default function Project() {
     return (
-        <div className={classes.projectDiv}></div>
+        <div className={classes.projectDiv}>
+            <h4 className={classes.h4}>Project not available yet</h4>
+        </div>
     );
 }
